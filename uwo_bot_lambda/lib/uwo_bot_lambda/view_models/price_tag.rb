326e6c5c163e9519acd7fa865fd3d5a9ef90543f
Chien-Fu Liu <35733299+chienfuliu@users.noreply.github.com>
2021-09-11 06:28:46 +0000

    Refactor how width is calculated and update the ratio to cater to Windows platforms.

--- uwo_bot_lambda/lib/uwo_bot_lambda/view_models/price_tag.rb
+++ uwo_bot_lambda/lib/uwo_bot_lambda/view_models/price_tag.rb
@@ -68,8 +68,19 @@ module UwoBotLambda
         PRICE_TABLE
       end
 
+      # An auxiliary class that helps text containing full-width characters,
+      # e.g. CJK characters, to be able to align with half-width characters such
+      # as Latin letters.
       class Text
-        FULL_SIZE_WHITESPACE = '　'
+        FULL_WIDTH_WHITESPACE = '　'
+        # Note that the ratio of full-width to half-width varies from device to
+        # device due to different font rendering strategies. Here we take visual
+        # result rendered on Windows as the standard as the game itself is
+        # runnable only on Windows platform and therefore most users use Windows
+        # as their bot client to see the rendered result.
+        # However, 5/3 is more accurate for other platforms, e.g. Linux, macOS.
+        # FULL_WIDTH_RATIO = Rational(5, 3)
+        FULL_WIDTH_RATIO = Rational(9, 5)
 
         def initialize(str)
           @raw = str.to_s.dup
@@ -77,17 +88,17 @@ module UwoBotLambda
 
         def width
           @width ||=
-            @raw.each_char.sum { |c| full_size?(c) ? Rational(5, 3) : 1 }
+            @raw.each_char.sum { |c| full_width?(c) ? FULL_WIDTH_RATIO : 1 }
         end
 
         def normalized?
-          width.to_i == width
+          width.denominator == 1
         end
 
         def normalized_width
           return width if normalized?
 
-          width + Rational(5, 3) * ((width * 3) % 3)
+          width + FULL_WIDTH_RATIO * counts_of_full_width_to_compensate
         end
 
         def to_s
@@ -97,25 +108,33 @@ module UwoBotLambda
         def ljust(width)
           return Text.new(@raw) if normalized_width > width
 
-          count_full = (self.width * 3) % 3
+          count_full = counts_of_full_width_to_compensate
           count_half = width - normalized_width
 
-          Text.new(@raw + FULL_SIZE_WHITESPACE * count_full + ' ' * count_half)
+          Text.new(@raw + FULL_WIDTH_WHITESPACE * count_full + ' ' * count_half)
         end
 
         def rjust(width)
           return Text.new(@raw) if normalized_width > width
 
-          count_full = (self.width * 3) % 3
+          count_full = counts_of_full_width_to_compensate
           count_half = width - normalized_width
 
-          Text.new(' ' * count_half + FULL_SIZE_WHITESPACE * count_full + @raw)
+          Text.new(' ' * count_half + FULL_WIDTH_WHITESPACE * count_full + @raw)
         end
 
         private
 
-        def full_size?(char)
-          char.bytesize > 2
+        # For simplicity, character with byte size greater than one is
+        # considered as full-width.
+        def full_width?(char)
+          char.bytesize > 1
+        end
+
+        def counts_of_full_width_to_compensate
+          width.denominator.times do |i|
+            return i if (width + FULL_WIDTH_RATIO * i).denominator == 1
+          end
         end
       end
     end
